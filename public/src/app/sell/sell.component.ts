5e33e5cd2668b20f59fc60a4a8b7cd33ab35ff03
Carl Bishop <carldbishop@aol.com>
2019-10-23 20:42:00 -0700

    Master

--- public/src/app/sell/sell.component.ts
+++ public/src/app/sell/sell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from './../http.service';
 
 @Component({
   selector: 'app-sell',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _httpService: HttpService) { }
 
   ngOnInit() {
   }
